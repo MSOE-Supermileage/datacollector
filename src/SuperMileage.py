8ebb87af3d5fbe27df72923fa7d62c5c41afc081
Connor Christie <chillercraft@hotmail.com>
2015-10-09 19:47:15 -0500

    Added new example speed

--- src/SuperMileage.py
+++ src/SuperMileage.py
@@ -1,7 +1,9 @@
 import json
+import time
 import socket
 import threading
 from time import sleep
+import math
 
 IP, PORT = ("192.168.1.2", 2004)
 
@@ -9,23 +11,21 @@ server_socket = None
 
 speed_data = {"data": "speed", "value": 20}
 rpm_data = {"data": "rpm", "value": 3500}
+time_data = {"data": "time", "value": int(time.time())}
 
 def handle_data():
-    going_up = True;
+    _time = 0
     
     while True:
-        if speed_data["value"] < 65 and going_up:
-            speed_data["value"] += 1
-        elif speed_data["value"] == 65 and going_up:
-            going_up = False
-        elif speed_data["value"] > 0 and not going_up:
-            speed_data["value"] -= 1
-        elif speed_data["value"] == 0 and not going_up:
-            going_up = True
-            
+        speed_data["value"] = int((60 / 2) * math.sin((1 / 2) * _time - (1 / 2) * math.pi) + 30)
+        
+        _time += 0.1
+        
+        time_data["value"] = int(time.time())
+        
         # Obtain sensor data from the GPIO and send it to the android app encoded in JSON
         
-        server_socket.sendto((json.dumps([speed_data, rpm_data]) + "\n").encode("utf-8"), (IP, PORT))
+        server_socket.sendto((json.dumps([speed_data, rpm_data, time_data]) + "\n").encode("utf-8"), (IP, PORT))
         
         sleep(0.1)
 
