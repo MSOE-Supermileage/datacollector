2cf4f2e23cd3f84bae70fa37eb1415447b499f06
Matthew Mahnke <mahnkem@msoe.edu>
2016-01-09 17:30:25 -0600

    Fix install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 function install_dependencies {
   apt-get update
-  [[ which python3 ]] && apt-get install -y python3
-  [[ which git ]] && apt-get install -y git
-  [[ which adb ]] && apt-get install -y android-tools-adb
-  [[ which arduino ]] && apt-get install -y arduino
+  [ `which python3` ] && apt-get install -y python3
+  [ `which git` ] && apt-get install -y git
+  [ `which adb` ] && apt-get install -y android-tools-adb
+  [ `which arduino` ] && apt-get install -y arduino
 }
 
 function copy_files {
-  [[ ! -d /usr/local/src/datacollector ]] && mkdir -p /usr/local/src/datacollector
-  ln -vis src/adb.py /usr/local/src/datacollector/adb.py
+  [ ! -d /usr/local/src/datacollector ] && mkdir -p /usr/local/src/datacollector
+  ln -vfs $PWD/src/adb.py /usr/local/src/datacollector/adb.py
 }
 
 function install_service {
-  cp -vf datacollector.service /etc/systemd/service/datacollector.service
-  systemctl daemon-restart
+  cp -vf datacollector.service /etc/systemd/system/datacollector.service
+  systemctl daemon-reload
   systemctl enable datacollector.service
 }
 
